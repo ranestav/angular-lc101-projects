d2921911fbd7d164c009b22f37dc4aee769850de
ranestav <christianh82995@gmail.com>
2021-04-12 13:44:24 -0500

    finished most of the bonuses

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -25,8 +25,17 @@ export class CandidatesComponent implements OnInit {
 
   // Code the addToCrew function here:
 
+  addToCrew(person: object) {
+    if(!this.crew.includes(person)) {
+      this.crew.push(person);
+    }
+    
+  }
 
   // BONUS: Code the changeMissionName function here:
+  changeMissionName (name: string) {
+    this.missionName = name;
+  }
 
 
 }
